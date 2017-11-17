d4c4d67ef715f07d043f3337b5195559eca93b9c
Nikita Zaitsev (exluap) <nickzaytsew@gmail.com>
2017-11-17 12:05:45 +0300

    add changes for import

--- start.go
+++ start.go
@@ -6,7 +6,7 @@ import (
     "log"
     "net/http"
     "strings"
-    "github.com/mdigger/translit"
+    "github.com/exluap/translit"
 
     ps "github.com/gorillalabs/go-powershell"
     "github.com/gorillalabs/go-powershell/backend"
