17231d653de7978c5e6244e6a9aa43c8a29b2897
Nikita Zaitsev (exluap) <nickzaytsew@gmail.com>
2018-03-23 01:55:26 +0300

    Added Logging to remote server. Added Path to OU in AD. Added Phone Number for Registration and adding to AD information

--- index.gtpl
+++ index.gtpl
@@ -2,12 +2,68 @@
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>CSS Validation</title>
+  <title>Форма регистрации</title>
   
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 
-  
-      <link rel="stylesheet" href="css/style.css">
+    <style type="text/css">
+    body {
+    	text-align: center;
+    	padding: 10px;
+    }
+
+    .form {
+    	display: inline-block;
+    	padding: 25px;
+    	border: 1px solid #ccc;
+    	border-radius: 5px;
+    	width: 500px;
+    }
+
+    .form__field {
+    	margin-bottom: 10px;
+    }
+
+    .form__error {
+    	color: red;
+    	text-align: left;
+    	font-size: 12px;
+    	display: block;
+    	margin-top: 3px;
+    	display: none;
+    }
+
+    .form input {
+    	outline: none;
+    	display: block;
+    	width: 100%;
+    	border-radius: 2px;
+    	border: 1px solid #ccc;
+    	padding: 10px;
+    	box-sizing: border-box;
+    }
+
+    .form button {
+    	width: 100%;
+    	padding: 10px;
+    	border-radius: 2px;
+    	border: 0;
+    	background-color: #ccc;
+    	color: #fff;
+    }
+
+    input:valid:not(:placeholder-shown) {
+    	border-color: green;
+    }
+
+    input:invalid:not(:placeholder-shown) {
+    	border-color: red;
+    }
+    input:invalid:not(:placeholder-shown) + .form__error {
+    	display: block;
+    }
+    </style>
+
 
   
 </head>
@@ -28,7 +84,7 @@
 		<span class="form__error">Это поле должно содержать E-Mail в формате example@site.com</span>
 	</div>
 	<div class="form__field">
-		<input type="tel" name="name" placeholder="Телефон*" pattern="[\+]\d{1}\s[\(]\d{3}[\)]\s\d{3}[\-]\d{2}[\-]\d{2}" minlength="18" maxlength="18" />
+		<input type="tel" name="tel" placeholder="Телефон*" pattern="[\+]\d{1}\s[\(]\d{3}[\)]\s\d{3}[\-]\d{2}[\-]\d{2}" minlength="18" maxlength="18" />
 		<span class="form__error">Это поле должно содержать телефон в формате +7 (123) 456-78-90</span>
 	</div>
 	<div class="form__field">
--- start.go
+++ start.go
@@ -1,83 +1,115 @@
 package main
 
 import (
-    "fmt"
-    "html/template"
-    "log"
-    "net/http"
-    "strings"
-    "github.com/exluap/translit"
+	"fmt"
+	"github.com/exluap/translit"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
 
-    ps "github.com/gorillalabs/go-powershell"
-    "github.com/gorillalabs/go-powershell/backend"
+	ps "github.com/gorillalabs/go-powershell"
+	"github.com/gorillalabs/go-powershell/backend"
+
+	"encoding/json"
+	"github.com/getsentry/raven-go"
+	"os"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm() //Parse url parameters passed, then parse the response packet for the POST body (request body)
-    // attention: If you do not call ParseForm method, the following data can not be obtained form
-    fmt.Println(r.Form) // print information on server side.
-    fmt.Println("path", r.URL.Path)
-    fmt.Println("scheme", r.URL.Scheme)
-    fmt.Println(r.Form["url_long"])
-    for k, v := range r.Form {
-        fmt.Println("key:", k)
-        fmt.Println("val:", strings.Join(v, ""))
-    }
-    fmt.Fprintf(w, "Hello astaxie!") // write data to response
+type Config struct {
+	LogServer struct {
+		DSN string `json:"DSN"`
+	} `json:"log_server"`
+}
+
+func LoadConfiguration(file string) Config {
+	var config Config
+	configFile, err := os.Open(file)
+	defer configFile.Close()
+	if err != nil {
+		raven.CaptureErrorAndWait(err, nil)
+		log.Println(err.Error())
+	}
+	jsonParser := json.NewDecoder(configFile)
+	jsonParser.Decode(&config)
+	return config
+}
+
+func Init() {
+	settings := LoadConfiguration("config.json")
+	raven.SetDSN(settings.LogServer.DSN)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("method:", r.Method) //get request method
-    if r.Method == "GET" {
-        t, _ := template.ParseFiles("index.gtpl")
-        t.Execute(w, nil)
-    } else {
-        r.ParseForm()
-        // logic part of log in
-        fmt.Println("fam:", r.Form["surname"])
-        fmt.Println("name:", r.Form["firstname"])
-        fmt.Println("otch:", r.Form["endname"])
-        fmt.Println("group:", r.Form["univergroup"])
-        fmt.Println("kaf:", r.Form["univerkafedra"])
-        fmt.Println("email:", r.Form["email"])
-        fmt.Println("pass:", r.Form["password"])
-       // fmt.Println(translit.Ru(strings.Join(r.Form["surname"], " ")))
-        fmt.Fprintf(w, "Твой логин: " + translit.Ru(strings.Join(r.Form["firstname"], " ")) + "." + translit.Ru(strings.Join(r.Form["surname"], " ")) + "\n")
-        fmt.Fprintf(w, "Пароль: Qwerty123" + " (этот пароль временный и работает только на первый вход) \n")
-        fmt.Fprintf(w, "Теперь ты можешь входить с этими данными в Windows и полноценно работать :) \n Хорошего тебе дня!")
-        fmt.Println("New-ADUser -Name '" + translit.Ru(strings.Join(r.Form["firstname"], " ")) + "." + translit.Ru(strings.Join(r.Form["surname"], " ")) + "' -AccountPassword(ConvertTo-SecureString 'Qwerty123' -AsPlainText -Force) -ChangePasswordAtLogon 1 -Company '" + translit.Ru(strings.Join(r.Form["univerkafedra"], " ")) + "' -Department '" + translit.Ru(strings.Join(r.Form["univergroup"], " ")) + "' -DisplayName '" + translit.Ru(strings.Join(r.Form["surname"], " ")) + " " + translit.Ru(strings.Join(r.Form["firstname"], " ")) +  " "+ translit.Ru(strings.Join(r.Form["endname"]," ")) + "' -EmailAddress '" + strings.Join(r.Form["email"], " ") + "' -Enabled 1 -GivenName '" + translit.Ru(strings.Join(r.Form["firstname"], " ")) + " " + translit.Ru(strings.Join(r.Form["endname"]," ")) + "' -SurName '" + translit.Ru(strings.Join(r.Form["surname"]," ")) + " " + "' -HomeDirectory '\\\\POSEIDON\\movedUsers\\%username%' -SamAccountName '" + translit.Ru(strings.Join(r.Form["firstname"], " ")) + "." + translit.Ru(strings.Join(r.Form["surname"], " ")) + "' -UserPrincipalName '" + translit.Ru(strings.Join(r.Form["firstname"], " ")) + "." + translit.Ru(strings.Join(r.Form["surname"], " ")) + "@ippo.mirea.ru'")
-        //fmt.Println("ADD-ADGroupMember 'students' –members '"+translit.Ru(strings.Join(r.Form["firstname"], " ")) + "." + translit.Ru(strings.Join(r.Form["surname"], " "))+"'")
-        
-        // choose a backend
-        back := &backend.Local{}
+	log.Println("method:", r.Method) //get request method
 
-        // start a local powershell process
-        shell, err := ps.New(back)
-        if err != nil {
-            fmt.Fprintf(w, "Извини, но что-то пошло не так. Позови администратора и он решит твой вопрос")
-            panic(err)
-        }
-        defer shell.Exit()
+	if r.Method == "GET" {
+		t, err := template.ParseFiles("index.gtpl")
+		err2 := t.Execute(w, nil)
 
-        // ... and interact with it
-        stdout, _, err := shell.Execute("New-ADUser -Name '" + translit.Ru(strings.Join(r.Form["surname"], " ")) + " " + translit.Ru(strings.Join(r.Form["firstname"], " ")) +  " " + translit.Ru(strings.Join(r.Form["endname"]," ")) + "' -AccountPassword(ConvertTo-SecureString 'Qwerty123' -AsPlainText -Force) -ChangePasswordAtLogon 1 -Company '" + translit.Ru(strings.Join(r.Form["univerkafedra"], " ")) + "' -Department '" + translit.Ru(strings.Join(r.Form["univergroup"], " ")) + "' -DisplayName '" + translit.Ru(strings.Join(r.Form["surname"], " ")) + " " + translit.Ru(strings.Join(r.Form["firstname"], " ")) +  " "+ translit.Ru(strings.Join(r.Form["endname"]," ")) + "' -EmailAddress '" + strings.Join(r.Form["email"], " ") + "' -Enabled 1 -GivenName '" + translit.Ru(strings.Join(r.Form["firstname"], " ")) + " " + translit.Ru(strings.Join(r.Form["endname"]," ")) + "' -SurName '" + translit.Ru(strings.Join(r.Form["surname"]," ")) + " " + "' -HomeDirectory '\\\\POSEIDON\\movedUsers\\%username%' -SamAccountName '" + translit.Ru(strings.Join(r.Form["firstname"], " ")) + "." + translit.Ru(strings.Join(r.Form["surname"], " ")) + "' -UserPrincipalName '" + translit.Ru(strings.Join(r.Form["firstname"], " ")) + "." + translit.Ru(strings.Join(r.Form["surname"], " ")) + "@ippo.mirea.ru'")
+		if err != nil {
+			raven.CaptureErrorAndWait(err, nil)
+			log.Panic(err)
+		}
 
-        if err != nil {
-            fmt.Fprintf(w, "Извини, но что-то пошло не так. Позови администратора и он решит твой вопрос")
-            panic(err)
-        }
+		if err2 != nil {
+			raven.CaptureErrorAndWait(err, nil)
+			log.Panic(err)
+		}
 
-        
-        fmt.Println(stdout) 
-    }
+	} else {
+		r.ParseForm()
+		log.Println("Получил данные, начинаю работать с Active Directory")
+		log.Println("method: ", r.Method)
+
+		// logic part of log in
+		/*
+		        log.Println("fam:", r.Form["surname"])
+		        log.Println("name:", r.Form["firstname"])
+		        log.Println("otch:", r.Form["endname"])
+		        log.Println("group:", r.Form["univergroup"])
+				log.Println("kaf:", r.Form["univerkafedra"])
+				log.Println("email:", r.Form["email"])
+				log.Println("pass:", r.Form["password"])**/
+
+		fmt.Fprintf(w, "Твой логин: "+translit.Ru(strings.Join(r.Form["firstname"], " "))+"."+translit.Ru(strings.Join(r.Form["surname"], " "))+"\n")
+		fmt.Fprintf(w, "Пароль: Qwerty123"+" (этот пароль временный и работает только на первый вход) \n")
+		fmt.Fprintf(w, "Теперь ты можешь входить с этими данными в Windows и полноценно работать :) \n Хорошего тебе дня!")
+		log.Println("New-ADUser -Name '" + translit.Ru(strings.Join(r.Form["firstname"], " ")) + "." + translit.Ru(strings.Join(r.Form["surname"], " ")) + "' -AccountPassword(ConvertTo-SecureString 'Qwerty123' -AsPlainText -Force) -ChangePasswordAtLogon 1 -Company '" + translit.Ru(strings.Join(r.Form["univerkafedra"], " ")) + "' -Department '" + translit.Ru(strings.Join(r.Form["univergroup"], " ")) + "' -DisplayName '" + translit.Ru(strings.Join(r.Form["surname"], " ")) + " " + translit.Ru(strings.Join(r.Form["firstname"], " ")) + " " + translit.Ru(strings.Join(r.Form["endname"], " ")) + "' -EmailAddress '" + strings.Join(r.Form["email"], " ") + "' -Enabled 1 -GivenName '" + translit.Ru(strings.Join(r.Form["firstname"], " ")) + " " + translit.Ru(strings.Join(r.Form["endname"], " ")) + "' -SurName '" + translit.Ru(strings.Join(r.Form["surname"], " ")) + " " + "' -ProfilePath '\\\\POSEIDON\\movedUsers$\\%username%' -SamAccountName '" + translit.Ru(strings.Join(r.Form["firstname"], " ")) + "." + translit.Ru(strings.Join(r.Form["surname"], " ")) + "' -UserPrincipalName '" + translit.Ru(strings.Join(r.Form["firstname"], " ")) + "." + translit.Ru(strings.Join(r.Form["surname"], " ")) + "@ippo.mirea.ru' -MobilePhone" + strings.Join(r.Form["tel"], " ") + " -Path 'OU=PersonaledUsers,DC=ippo,DC=mirea,DC=ru")
+
+		// choose a backend
+		back := &backend.Local{}
+
+		// start a local powershell process
+		shell, err := ps.New(back)
+		if err != nil {
+			fmt.Fprintf(w, "Извини, но что-то пошло не так. Позови администратора и он решит твой вопрос")
+			raven.CaptureErrorAndWait(err, nil)
+			log.Panic(err)
+		}
+		defer shell.Exit()
+
+		// ... and interact with it
+		log.Println("Пытаюсь через PowerShell добавить пользователя")
+		stdout, _, err := shell.Execute("New-ADUser -Name '" + translit.Ru(strings.Join(r.Form["firstname"], " ")) + "." + translit.Ru(strings.Join(r.Form["surname"], " ")) + "' -AccountPassword(ConvertTo-SecureString 'Qwerty123' -AsPlainText -Force) -ChangePasswordAtLogon 1 -Company '" + translit.Ru(strings.Join(r.Form["univerkafedra"], " ")) + "' -Department '" + translit.Ru(strings.Join(r.Form["univergroup"], " ")) + "' -DisplayName '" + translit.Ru(strings.Join(r.Form["surname"], " ")) + " " + translit.Ru(strings.Join(r.Form["firstname"], " ")) + " " + translit.Ru(strings.Join(r.Form["endname"], " ")) + "' -EmailAddress '" + strings.Join(r.Form["email"], " ") + "' -Enabled 1 -GivenName '" + translit.Ru(strings.Join(r.Form["firstname"], " ")) + " " + translit.Ru(strings.Join(r.Form["endname"], " ")) + "' -SurName '" + translit.Ru(strings.Join(r.Form["surname"], " ")) + " " + "' -ProfilePath '\\\\POSEIDON\\movedUsers$\\%username%' -SamAccountName '" + translit.Ru(strings.Join(r.Form["firstname"], " ")) + "." + translit.Ru(strings.Join(r.Form["surname"], " ")) + "' -UserPrincipalName '" + translit.Ru(strings.Join(r.Form["firstname"], " ")) + "." + translit.Ru(strings.Join(r.Form["surname"], " ")) + "@ippo.mirea.ru' -MobilePhone" + strings.Join(r.Form["tel"], " ") + " -Path 'OU=PersonaledUsers,DC=ippo,DC=mirea,DC=ru")
+
+		if err != nil {
+			fmt.Fprintf(w, "Извини, но что-то пошло не так. Позови администратора и он решит твой вопрос")
+			raven.CaptureErrorAndWait(err, map[string]string{"Step": "execute Powershell"})
+			log.Panic(err)
+		}
+
+		log.Println(stdout)
+	}
 }
 
 func main() {
-    http.Handle("/css/", http.FileServer(http.Dir("./css/")))
-    http.HandleFunc("/", login) // setting router rule
-    http.HandleFunc("/register", login)
-    err := http.ListenAndServe(":9090", nil) // setting listening port
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	Init()
+	http.HandleFunc("/", login) // setting router rule
+	http.HandleFunc("/register", login)
+	err := http.ListenAndServe(":9090", nil) // setting listening port
+	if err != nil {
+		raven.CaptureErrorAndWait(err, nil)
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
